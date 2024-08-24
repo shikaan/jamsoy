98492ec221a91b3786353e1f889fb28a996f2a2d
shikaan <spagnolo.manu@gmail.com>
2024-08-24 11:53:59 +0200

    feat: make gamboy independent of canvas

--- public/gameboy.mjs
+++ public/gameboy.mjs
@@ -5,16 +5,15 @@ import { Timer } from "../lib/timer.mjs";
 import { register } from "../lib/cpu/registers.mjs";
 import { Interrupts } from "../lib/interrupts.mjs";
 import { Graphics } from "../lib/graphics.mjs";
-import { CanvasScreen } from "../lib/screen.mjs";
 
 class GameBoy {
-  constructor(canvas) {
+  constructor(screen) {
     this.memory = new Memory();
     this.register = register;
     this.interrupts = new Interrupts(this.memory, register);
     this.cpu = new CPU(this.memory, register, this.interrupts);
     this.timer = new Timer(this.memory, this.interrupts);
-    this.screen = new CanvasScreen(canvas);
+    this.screen = screen
     this.graphics = new Graphics(this.memory, this.interrupts, this.screen);
 
     let buffer = '';
@@ -36,11 +35,14 @@ class GameBoy {
     this.#init();
   }
 
+  instructions = 0;
+
   update() {
     let cycles = 0;
     while (cycles < CPU.MAX_CYCLES) {
       this.interrupts.handleInterrupts();
       cycles += this.cpu.executeNextIntruction(false);
+      this.instructions++;
       this.timer.update(cycles);
       this.graphics.update(cycles);
     }
