f55d14d08de63e082cbab92a0a347b5afe731afb
shikaan <spagnolo.manu@gmail.com>
2024-08-31 23:42:44 +0200

    chore: release

--- docs/lib/gfx/screen.mjs
+++ docs/lib/gfx/screen.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
 class Screen {
-  setPixel(x, y, uint32) {
+  setPixel(x, y, Uint8ClampedArray) {
     throw new Error("Not implemented");
   }
 
@@ -14,41 +14,4 @@ class Screen {
   }
 }
 
-class CanvasScreen extends Screen {
-  #context;
-  #imageData = new ImageData(160, 144);
-
-  /**
-   * @param {HTMLCanvasElement} canvas
-   */
-  constructor(canvas) {
-    super();
-    const context = canvas.getContext("2d");
-
-    if (!context) {
-      throw new Error("Could not get 2d context from canvas");
-    }
-
-    this.#context = context;
-  }
-
-  /**
-   * @param {number} x
-   * @param {number} y
-   * @param {Uint8ClampedArray} color
-   */
-  setPixel(x, y, color) {
-    this.#imageData.data.set(color, (x + y * 160) * 4);
-  }
-
-  draw() {
-    this.#context.putImageData(this.#imageData, 0, 0);
-  }
-
-  clear() {
-    this.#imageData = new ImageData(160, 144);
-    this.draw();
-  }
-}
-
-export { CanvasScreen, Screen };
+export { Screen };
